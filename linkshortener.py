c37c55906b08dc7203a6d19f7ac655a9adb4272f
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-03-13 23:36:55 +0530

    Update linkshortener.py

--- linkshortener.py
+++ linkshortener.py
@@ -3,4 +3,4 @@ import pyshorteners
 url = input("Please Enter the URL: ")
 
 s = pyshorteners.Shortener()
-print("Your Shortened URL is -> ",s.tinyurl.short(url))
+print("Your Shortened URL is here -> ",s.tinyurl.short(url))
